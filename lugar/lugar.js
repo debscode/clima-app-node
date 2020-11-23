e84bd85715e35c203b1c436aec206a81ce88e8f4
debscode <52897527+debscode@users.noreply.github.com>
2020-11-23 18:41:08 -0500

    Update lugar.js

--- lugar/lugar.js
+++ lugar/lugar.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getLugarLatLng = async (direccion) => {
     const encodeUrl = encodeURI(direccion);
-    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${encodeUrl}&appid=6727392200160a23cebe5c1d912aa955`);
+    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${encodeUrl}&appid={key}`);
 
     if (response.status != 200) {
         throw new Error(`No hay datos para la ciudad ${direccion}`);
