3a03cc99fcd8adbb3eb5a206581766b533e98f07
debscode <52897527+debscode@users.noreply.github.com>
2020-11-23 18:41:32 -0500

    Update clima.js

--- clima/clima.js
+++ clima/clima.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const getClima = async (lat, lng) => {    
-    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=6727392200160a23cebe5c1d912aa955&units=metric`);
+    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid={key}&units=metric`);
 
     if (response.status != 200) {
         throw new Error(`No hay datos para la ciudad ${lat} ${lng}`);
